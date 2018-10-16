cd421cf0e31ad80eb1f5afb88263c31a54fc4988
Timothy Wilson <blaareproductions@gmail.com>
2018-10-16 15:59:42 -0400

    added initial proxy function

--- proxy.php
+++ proxy.php
@@ -1,9 +1,39 @@
-<?php echo '<p>Hello World</p>'; ?>
-/**
- * Created by PhpStorm.
- * User: twilson
- * Date: 10/16/18
- * Time: 2:23 PM
- */
+<?php
 
-//echo $_GET["request_name"];
+    if ( $_GET["authenticate"]) {
+
+        return "testing";
+        $partnerName = "applicant";
+        $partnerPassword = "d7c3119c6cdab02d68d9";
+        $partnerUserID = "expensifytest@mailinator.com";
+        $password = "hire_me";
+
+        httpPost($partnerName, $partnerPassword, $partnerUserID, $password);
+        exit();
+    }
+
+
+
+    function httpPost($partnerName, $partnerPassword, $partnerUserID,$password){
+       $url = "https://www.expensify.com/api?command=Authenticate";
+
+       $fields = [
+           "partnerName"        => $partnerName,
+           "partnerPassword"    => $partnerPassword,
+           "partnerUserID"      => $partnerUserID,
+           "password"           => $password
+       ];
+
+       $fields_string = http_build_query($fields);
+
+       $ch = curl_init();
+
+       curl_setopt($ch, CURLOPT_URL, $url);
+       curl_setopt($ch, CURLOPT_POST, count($fields));
+       curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+
+       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+       $result = curl_exec($ch);
+       echo $result;
+    }
--- script.js
+++ script.js
@@ -1,1 +1,61 @@
-console.log('Hello World!');
+checkAuthToken();
+
+
+function setCookie(cname, cvalue) {
+    document.cookie = cname + "=" + cvalue + ";" + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkAuthToken() {
+    var authToken = getCookie("authToken");
+    if (authToken != "") {
+        alert("user is logged in");
+    } else {
+        alert("user is not logged in");
+    }
+}
+
+/**
+ * Function loginToExpensify
+ * Goal:    to allow the user to log in to the Expensify API.
+ * Required:    partnerName
+ *              partnerPassword
+ *              partnerUserID
+ *              partnerUserSecret
+ *  POST via https://www.expensify.com/api?command=Authenticate
+ */
+function loginToExpensify(){
+
+    var data = {
+        partnerName:        "applicant",
+        partnerPassword:    "d7c3119c6cdab02d68d9",
+        partnerUserID:      "expensifytest@mailinator.com",
+        password:           "hire_me"
+    };
+
+    console.log("User Is Attempting to Log in");
+    $.ajax({
+        type: "GET",
+        url: "./proxy.php?authenticate",
+        success: function(success){
+            alert(success);
+        }
+    });
+
+}
+
+loginToExpensify();
