2e015904591973bb62c7429159ecf98723716726
Timothy Wilson <blaareproductions@gmail.com>
2018-10-17 19:36:23 -0400

    added consolelog

--- script.js
+++ script.js
@@ -284,6 +284,7 @@ function createTransaction(create, amt, merch){
         success: function(success){
             console.log(success);
             var transaction = JSON.parse(success);
+            console.log(transaction);
             $("#transactionTableBody").prepend(pullRequiredFields(transaction.transactionList));
         },
         error: function(error){
